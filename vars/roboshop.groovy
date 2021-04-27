12f354b4598b078ae6535dcef896426855589d9f
KalluruS <>
2021-04-27 19:02:50 +0530

    Update jenkins/

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,16 +1,21 @@
 def call(Map params = [:]) {
   // Start Default Arguments
   def args = [
-          NEXUS                  : 'some',
+          NEXUS_IP               : '172.31.14.124',
   ]
   args << params
 
   // End Default + Required Arguments
   pipeline {
-    agent any
+    agent {
+      label "${args.SLAVE_LABEL}"
+    }
 
     environment {
-      COMPONENT = "${args.COMPONENT}"
+      COMPONENT     = "${args.COMPONENT}"
+      NEXUS_IP      = "${args.NEXUS_IP}"
+      PROJECT_NAME  = "${args.PROJECT_NAME}"
+      SLAVE_LABEL   = "${args.SLAVE_LABEL}"
     }
 
     stages {
@@ -28,7 +33,7 @@ def call(Map params = [:]) {
       stage('Upload Artifacts') {
         steps {
           sh '''
-          curl -f -v -u admin:admin123 --upload-file frontend.zip http://172.31.14.124:8081/repository/frontend/frontend.zip
+          curl -f -v -u admin:admin123 --upload-file frontend.zip http://${NEXUS_IP}:8081/repository/frontend/frontend.zip
         '''
         }
       }
