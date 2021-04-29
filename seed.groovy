bd6b4cfb8f5853d4bcf1e127daad58a38a209f29
KalluruS <>
2021-04-29 18:57:37 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -28,6 +28,14 @@ pipelineJob('CI-Pipelines/frontend-ci') {
 
 pipelineJob('CI-Pipelines/catalogue-ci') {
   configure { flowdefinition ->
+    flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+      'triggers' {
+        'hudson.triggers.SCMTrigger' {
+          'spec'('* * * * 1-5')
+          'ignorePostCommitHooks'(false)
+        }
+      }
+    }
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
