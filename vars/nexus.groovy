f3a7bbd1cc25df52d8a3079eaf661c1791d90657
KalluruS <>
2021-04-29 19:44:48 +0530

    Update jenkins/

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -27,7 +27,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
 
 def code_build(APP_TYPE, COMPONENT) {
   if(APP_TYPE == "NODEJS") {
-    //command = "npm install"
+    command = "npm install"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "JAVA") {
