375f768ede0855374fd4ff10c24840092ccf73b8
KalluruS <>
2021-04-29 19:02:01 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -5,7 +5,7 @@ folder('CI-Pipelines') {
 
 def component = ["cart", "catalogue"];
 
-for(int i in component) {
+for(String i in component) {
   pipelineJob("test-${i}") {
     configure { flowdefinition ->
       flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
