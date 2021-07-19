2fb9f669cf40409aa022917bccb3bafb577e11d9
KalluruS <>
2021-07-19 19:34:27 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -127,3 +127,24 @@ pipelineJob("Kubernetes/MySQL-Schema") {
     }
   }
 }
+
+pipelineJob("Kubernetes/Frontend") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/zsdevops01/frontend.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile-K8S')
+      'lightweight'(true)
+    }
+  }
+}
