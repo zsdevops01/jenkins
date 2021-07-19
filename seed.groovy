2706e904463e21e1d172337dac341be4484380a7
KalluruS <>
2021-07-19 19:10:56 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -106,3 +106,24 @@ pipelineJob("Kubernetes/MongoDB-Schema") {
     }
   }
 }
+
+pipelineJob("Kubernetes/MySQL-Schema") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/zsdevops01/mysql.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
