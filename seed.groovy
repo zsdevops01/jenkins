4a667df8f7869877bd819ce3e242078cd64e0b52
KalluruS <>
2021-07-30 18:55:31 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -253,3 +253,24 @@ pipelineJob("Kubernetes/Payment") {
     }
   }
 }
+
+pipelineJob("CI-CD-Test") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/zsdevops01/frontend.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile-cicd')
+      'lightweight'(true)
+    }
+  }
+}
